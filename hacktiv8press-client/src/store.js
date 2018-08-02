f8657422566074756b5bc4114eee98136080eb73
Tushar <tusharbedi7373@hotmail.com>
2018-08-02 17:09:33 +0700

    server deployment

---
+++ hacktiv8press-client/src/store.js
@@ -0,0 +1,207 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+import axios from 'axios'
+import swal from 'sweetalert'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    username: '',
+    articles: '',
+    isLoggedIn: ''
+  },
+  mutations: {
+    username (state, payload) {
+      state.username = payload
+    },
+    articles (state, payload) {
+      state.articles = payload
+    },
+    isLoggedIn (state, payload) {
+      state.isLoggedIn = payload
+    }
+  },
+  actions: {
+    getLogin ({ commit }, payload) {
+      axios
+        .post('http://localhost:8081/login', payload)
+        .then(({ data }) => {
+          if (data.token) {
+            commit('isLoggedIn', true)
+            commit('username', data.username)
+            localStorage.setItem('username', data.username)
+            localStorage.setItem('token', data.token)
+            router.push('/')
+            swal({
+              title: 'Login successful!',
+              text: 'Please feel free to post an article',
+              icon: 'success'
+            })
+          } else {
+            swal({
+              title: 'Error!',
+              text: 'Wrong Username / Password',
+              icon: 'error'})
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    getRegister ({ commit }, payload) {
+      axios
+        .post('http://localhost:8081/register', payload)
+        .then(({ data }) => {
+          if (data.token) {
+            commit('isLoggedIn', true)
+            commit('username', data.username)
+            localStorage.setItem('username', data.username)
+            localStorage.setItem('token', data.token)
+            router.push('/')
+            swal({
+              title: 'Register successful!',
+              text: 'Please feel free to post an article',
+              icon: 'success'
+            })
+          } else {
+            swal({
+              title: 'Error!',
+              text: 'Please fill all boxes',
+              icon: 'error'})
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    logout ({ commit }) {
+      localStorage.removeItem('token')
+      localStorage.removeItem('username')
+      commit('isLoggedIn', false)
+      commit('username', '')
+      router.push('/login')
+      swal({
+        text: 'You are logged out!',
+        icon: 'success'
+      })
+    },
+    statusLoggedIn ({ commit }) {
+      commit('isLoggedIn', true)
+      commit('username', localStorage.getItem('username'))
+    },
+    getArticles ({ commit }, payload) {
+      let token = localStorage.getItem('token')
+      let config = { headers: { token } }
+      axios
+        .get('http://localhost:8081/article', config)
+        .then(({ data }) => {
+          commit('articles', data.articles)
+        })
+        .catch(err => {
+          console.log(err)
+          swal({
+            title: 'Error!',
+            text: 'Our Server is currently down. Please wait a moment'
+          })
+        })
+    },
+    getOneArticle ({ commit }, payload) {
+      let token = localStorage.getItem('token')
+      let config = { headers: { token } }
+      axios
+        .get(`http://localhost:8081/article/${payload}`, config)
+        .then(({ data }) => {
+          commit('articles', data.article)
+        })
+        .catch(err => {
+          console.log(err)
+          swal({
+            title: 'Error!',
+            text: 'Our Server is currently down. Please wait a moment'
+          })
+        })
+    },
+    addArticle (context, payload) {
+      let token = localStorage.getItem('token')
+      let config = { headers: { token } }
+      axios
+        .post('http://localhost:8081/article', payload, config)
+        .then(({ data }) => {
+          if (data.body) {
+            swal({
+              title: 'Warning!',
+              text: 'Please fill all of the boxes',
+              icon: 'error'
+            })
+          } else {
+            router.push(`/article/${data.newArticle._id}`)
+            swal({
+              title: 'Successfully add new article',
+              icon: 'success'
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    updateArticle: function (context, payload) {
+      let token = localStorage.getItem('token')
+      let config = { headers: { token } }
+      axios
+        .put(`http://localhost:8081/article/${payload.id}`, payload, config)
+        .then(({ data }) => {
+          context.dispatch('getArticles')
+          swal({
+            title: 'Successfully update your article',
+            icon: 'success'
+          })
+        })
+        .catch(err => {
+          console.log(err)
+          swal({
+            title: 'Warning!',
+            text: JSON.stringify(err),
+            icon: 'error'
+          })
+        })
+    },
+    deleteArticle: function (context, payload) {
+      let token = localStorage.getItem('token')
+      let config = { headers: { token } }
+      swal({
+        title: 'Are you sure?',
+        text: 'Once deleted, you will not be able to recover this article!',
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true
+      })
+        .then((willDelete) => {
+          if (willDelete) {
+            axios
+              .delete(`http://localhost:8081/article/${payload}`, config)
+              .then(({ data }) => {
+                if (data.body) {
+                  swal({
+                    title: 'Warning!',
+                    text: JSON.stringify(data),
+                    icon: 'error'
+                  })
+                } else {
+                  swal({
+                    text: 'Successfully delete article!',
+                    icon: 'success'
+                  })
+                  context.dispatch('getArticles')
+                }
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          }
+        })
+    }
+  }
+})
---
+++ hacktiv8press-client/src/views/Home.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="home">
+    <Nav></Nav>
+    <h3>List of Articles</h3>
+    <br>
+    <text-box-modal></text-box-modal>
+    <br>
+    <hr>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div v-for="(article, index) in articles" :key="index" class="post-preview">
+            <router-link :to="{ path: `/article/${article._id}` }">
+              <h2 class="post-title">
+                {{ article.title }}
+              </h2>
+            </router-link>
+            <p class="post-meta">Posted by
+              <router-link :to="{ path: `/author/${article.author._id}` }">
+              {{ article.author.username }}
+              </router-link>
+              on {{ getDate(article.createdAt) }}</p>
+            <p>Category:
+              <router-link :to="{ path: `/category/${article.category}` }">
+                {{ article.category }}
+              </router-link>
+            </p>
+            <hr>
+            <vue-editor v-html="article.content"></vue-editor>
+            <hr>
+            <text-box-modal v-if="article.author.username === username"
+            :id="article._id" :item="article"></text-box-modal>
+            <hr class="hr-border">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Nav from '@/components/Nav.vue'
+import TextBoxModal from '@/components/TextBoxModal.vue'
+import { mapState, mapActions } from 'vuex'
+import moment from 'moment'
+import { VueEditor } from 'vue2-editor'
+export default {
+  name: 'home',
+  components: {
+    Nav,
+    TextBoxModal,
+    VueEditor
+  },
+  methods: {
+    ...mapActions([
+      'getArticles',
+      'statusLoggedIn'
+    ]),
+    getDate (date) {
+      return moment(date).format('DD MMMM [at] HH:mm')
+    }
+  },
+  computed: {
+    ...mapState({
+      articles: 'articles',
+      isLoggedIn: 'isLoggedIn',
+      username: 'username'
+    })
+  },
+  created () {
+    this.getArticles()
+    if (localStorage.getItem('token')) {
+      this.statusLoggedIn()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.post-preview {
+  a {
+    color: darkcyan;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  a:hover {
+    color: lightgreen;
+    text-decoration: none;
+  }
+  .btn {
+    margin: 10px;
+  }
+}
+.hr-border {
+    display: block;
+    height: 1px;
+    border: 0;
+    border-top: 2px solid black;
+    margin: 1em 0;
+    padding: 0;
+}
+</style>
